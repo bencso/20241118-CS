11f260c3418a24c15c07998ea2b96c5327d9480d
Bencso <158219939+bencso@users.noreply.github.com>
2024-11-18 09:23:30 +0100

    🚀 First commit

---
+++ Ber.cs
@@ -0,0 +1,26 @@
+﻿namespace berek_babolnaibence;
+
+internal class Ber
+{
+    public string Nev { get; set; }
+    public bool Nem { get; set; }
+    public string Reszleg { get; set; }
+    public int Belepes { get; set; }
+    public int BerFizetes { get; set; }
+
+    public override string ToString()
+    {
+        return $"\n\tNév: {Nev}\n\tNem: {(Nem ? "férfi" : "nő")}\n\tBelépés: {Belepes}\n\tBér: {string.Format("{0:N0}", BerFizetes)} Forint";
+    }
+
+    public Ber(string sor)
+    {
+        string[] parts = sor.Split(";");
+        Nev = parts[0];
+        Nem = parts[1] == "férfi";
+        Reszleg = parts[2];
+        Belepes = int.Parse(parts[3]);
+        BerFizetes = int.Parse(parts[4]);
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using berek_babolnaibence;
+using System.Text;
+
+List<Ber> berek2020 = new();
+StreamReader sr = new StreamReader("@/../../../../src/berek2020.txt", encoding: Encoding.UTF8);
+
+sr.ReadLine();
+while (!sr.EndOfStream) berek2020.Add(new Ber(sr.ReadLine()!));
+
+Console.WriteLine($"3. feladat: Dolgozók száma: {berek2020.Count} fő");
+
+Console.WriteLine($"4. feladat: Bérek átlag: {Math.Round(berek2020.Average(p=>p.BerFizetes)/1000,1)} e Ft");
+
+Console.Write("5. feladat: Kérem a részleg nevét: ");
+
+string reszlegNeve = Console.ReadLine()!.ToLower();
+if (berek2020.Where(p=>p.Reszleg.ToLower()==reszlegNeve).Count() > 0) 
+    Console.WriteLine($"6.feladat: A legtöbbet kereső dolgozó a megadott {reszlegNeve} részlegen: " +
+        $"{berek2020.Where(p => p.Reszleg == reszlegNeve).MaxBy(p => p.BerFizetes)!.ToString()}");
+else Console.WriteLine("6. feladat: A megadott részleg nem létezik a cégnél!");
+
+var berekReszleg = berek2020.GroupBy(p => p.Reszleg);
+Console.WriteLine($"7. feladat: Statisztika:");
+foreach (var item in berekReszleg) Console.WriteLine($"{item.Key,15}: {item.Count(),5} fő");
